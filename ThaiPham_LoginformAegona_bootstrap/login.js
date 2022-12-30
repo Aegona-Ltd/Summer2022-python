c5f3244a5cbd1580805378af5d38d3006897b659
thaipham7820 <thai.pham@aegona.com>
2022-12-30 11:05:52 +0700

    update vaidation

--- ThaiPham_LoginformAegona_bootstrap/login.js
+++ ThaiPham_LoginformAegona_bootstrap/login.js
@@ -1,166 +1,179 @@
 // // Selectors
-// let loginFields = document.querySelectorAll("#loginForm input");
-// let loginButton = document.querySelector("#loginbtn");
-// let loginForm = document.querySelector("#loginForm");
-// let emptyUnameMessage = document.querySelector(".empty-username-feedback");
-// let emptyPasswdMessage = document.querySelector(".empty-password-feedback");
-// let invalUnameMessage = document.querySelector(".invalid-username-feedback");
-// let invalPasswdMessage = document.querySelector(".invalid-password-feedback");
+let submitFields = document.querySelectorAll("#submitForm input");
+let messageArea = document.querySelector("#message");
+let submitButton = document.querySelector("#submitbtn");
+let submitForm = document.querySelector("#submitForm");
+let emptyNameMessage = document.querySelector(".empty-fullname-feedback");
+let emptyCompanyNameMessage = document.querySelector(
+  ".empty-companyname-feedback"
+);
+let invalNameMessage = document.querySelector(".invalid-fullname-feedback");
+let invalCompanyNameMessage = document.querySelector(
+  ".invalid-companyname-feedback"
+);
+
+let emptyBizPhoneMessage = document.querySelector(
+  ".empty-businessphone-feedback"
+);
+let emptyEmailMessage = document.querySelector(".empty-email-feedback");
+let invalBizPhoneMessage = document.querySelector(
+  ".invalid-businessphone-feedback"
+);
+let invalEmailMessage = document.querySelector(".invalid-email-feedback");
+let emptyMessage = document.querySelector(".empty-message-feedback");
+let invalMessage = document.querySelector(".invalid-message-feedback");
+
 // let maintittle = document.querySelector(".maintitle");
 let formLabels = document.querySelectorAll(".form-label");
 
 // // Operational Valiables
 let config = {
-  // uname: "uname",
-  // password: "pwd",
-  // displaynone: "none",
-  // displayblock: "block",
+  fName: "uname",
+  password: "pwd",
+  displaynone: "none",
+  displayblock: "block",
   formLabel: "form-label",
   clickedFormLabel: "clicked-form-label",
 };
 
 let inputTypes = {
-  // input: "input",
-  // click: "click",
+  input: "input",
+  click: "click",
   mouseDown: "mousedown",
 };
 
-// let condition = {
-//   truth: true,
-//   unTruth: false,
-// };
+let condition = {
+  truth: true,
+  unTruth: false,
+};
 
-// let textData = {
-//   empty: "",
-//   sucessLogin: "Login Suceccfully!",
-//   userName: "userName",
-//   password: "password",
-//   invalid: "Invalid",
-//   emptyText: "Empty",
-// };
+let textData = {
+  empty: "",
+  sucessLogin: "Login Suceccfully!",
+  fName: "Full_Name",
+  cName: "Company_Name",
+  bizPhone: "Business_Phone",
+  email: "Email",
+  message: "Message",
+  invalid: "Invalid",
+  emptyText: "Empty",
+};
 
-// let errorDict = {
-//   userNameEmpty: emptyUnameMessage,
-//   passwordEmpty: emptyPasswdMessage,
-//   userNameInvalid: invalUnameMessage,
-//   passwordInvalid: invalPasswdMessage,
-// };
+let errorDict = {
+  Full_NameEmpty: emptyNameMessage,
+  Company_NameEmpty: emptyCompanyNameMessage,
+  Business_PhoneEmpty: emptyBizPhoneMessage,
+  EmailEmpty: emptyEmailMessage,
+  EmailInvalid: invalEmailMessage,
+  MessageEmpty: emptyMessage,
+  // userNameInvalid: invalUnameMessage,
+  // passwordInvalid: invalPasswdMessage,
+};
 
-// let dataHolder = [];
-// let validateResults = [];
-// let loginStatus = condition.unTruth;
+let dataHolder = [];
+let validateResults = [];
+let submitStatus = condition.unTruth;
 // // Logic
 // /*
-// When press login buttton : handleLogin will run first.
+// When press submit buttton : handleSubmit will run first.
 // */
 
-// function getLoginData() {
-//   let userNameVal = loginFields[0].value;
+function getFormData() {
+  for (let i = 0; i < submitFields.length; i++) {
+    let item = {
+      fieldName: submitFields[i].name,
+      value: submitFields[i].value,
+    };
+    dataHolder.push(item);
+  }
+  let messageVal = messageArea.value;
+  let item = { fieldName: textData.message, value: messageVal };
+  dataHolder.push(item);
+}
 
-//   // let userName = e.target.value;
-//   let item1 = { fieldName: textData.userName, value: userNameVal };
-//   dataHolder.push(item1);
+function validateEmail(mail) {
+  let pattern =
+    /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
+  return pattern.test(mail);
+}
 
-//   let passwordVal = loginFields[1].value;
-//   // dataHolder.password = password;
-//   let item2 = { fieldName: textData.password, value: passwordVal };
-//   dataHolder.push(item2);
-// }
+function validate(fieldName) {
+  let item = dataHolder.filter((ele) => ele.fieldName == fieldName)[0];
+  let itemName = item.fieldName;
+  let itemVal = item.value;
 
-// function validateEmail(mail) {
-//   let pattern =
-//     /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
-//   return pattern.test(mail);
-// }
+  switch (itemName) {
+    case textData.email:
+      if (!validateEmail(itemVal)) {
+        let data = itemName + textData.invalid;
+        validateResults.push(data);
+      }
+      break;
+    default:
+  }
+}
 
-// function validatePassword(passwd) {
-//   // 8 letter password, with at least a symbol, upper and lower case letters and a number
-//   let pattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-//   return pattern.test(passwd);
-// }
+function validateEmpty(data) {
+  let emptyLog = [];
+  for (let i = 0; i < data.length; i++) {
+    let fieldVal = data[i].value;
+    let fieldName = data[i].fieldName;
+    if (fieldVal) {
+      let item1 = { field: fieldName, mode: condition.truth };
+      emptyLog.push(item1);
+    } else {
+      let item2 = { field: fieldName, mode: condition.unTruth };
+      emptyLog.push(item2);
+    }
+  }
+  return emptyLog;
+}
 
-// function validate(fieldName) {
-//   let item = dataHolder.filter((ele) => ele.fieldName == fieldName)[0];
-//   let itemName = item.fieldName;
-//   let itemVal = item.value;
+function displayErrorsMessage() {
+  resetErrorsMessage();
+  for (let i = 0; i < validateResults.length; i++) {
+    let item = validateResults[i];
+    console.log(item);
+    errorDict[item].style.display = config.displayblock;
+  }
+}
 
-//   switch (itemName) {
-//     case textData.userName:
-//       if (!validateEmail(itemVal)) {
-//         let data = itemName + textData.invalid;
-//         validateResults.push(data);
-//       }
-//       break;
-//     case textData.password:
-//       if (!validatePassword(itemVal)) {
-//         let data = itemName + textData.invalid;
-//         validateResults.push(data);
-//       }
-//       break;
-//     default:
-//   }
-// }
+function resetErrorsMessage() {
+  let arr=[emptyNameMessage,emptyCompanyNameMessage,emptyBizPhoneMessage,emptyEmailMessage,emptyMessage,invalEmailMessage];
+  for (let i = 0; i < arr.length; i++) {
+     arr[i].style.display = config.displaynone;
+  }
+}
 
-// function validateEmpty(data) {
-//   let emptyLog = [];
-//   for (let i = 0; i < data.length; i++) {
-//     let fieldVal = data[i].value;
-//     let fieldName = data[i].fieldName;
-//     if (fieldVal) {
-//       let item1 = { field: fieldName, mode: condition.truth };
-//       emptyLog.push(item1);
-//     } else {
-//       let item2 = { field: fieldName, mode: condition.unTruth };
-//       emptyLog.push(item2);
-//     }
-//   }
-//   return emptyLog;
-// }
+function handleSubmit(e) {
+  dataHolder = [];
+  validateResults = [];
+  e.preventDefault();
+  getFormData(e);
+  let emptyResult = validateEmpty(dataHolder);
 
-// function displayErrorsMessage() {
-//   resetErrorsMessage();
-//   for (let i = 0; i < validateResults.length; i++) {
-//     let item = validateResults[i];
-//     errorDict[item].style.display = config.displayblock;
-//   }
-// }
+  for (let i = 0; i < emptyResult.length; i++) {
+    let emptyItem = emptyResult[i].mode;
+    let itemName = emptyResult[i].field;
+    if (!emptyItem) {
+      let data = itemName + textData.emptyText;
+      validateResults.push(data);
+    }
+    if (emptyItem) {
+      validate(itemName);
+    }
+  }
 
-// function resetErrorsMessage() {
-//   emptyUnameMessage.style.display = config.displaynone;
-//   emptyPasswdMessage.style.display = config.displaynone;
-//   invalUnameMessage.style.display = config.displaynone;
-//   invalPasswdMessage.style.display = config.displaynone;
-// }
+  displayErrorsMessage();
+  if (validateResults.length == 0) {
+    submitStatus = condition.truth;
+  }
 
-// function handleLogin(e) {
-//   dataHolder = [];
-//   validateResults = [];
-//   e.preventDefault();
-//   getLoginData(e);
-//   let emptyResult = validateEmpty(dataHolder);
-
-//   for (let i = 0; i < emptyResult.length; i++) {
-//     let emptyItem = emptyResult[i].mode;
-//     let itemName = emptyResult[i].field;
-//     if (!emptyItem) {
-//       let data = itemName + textData.emptyText;
-//       validateResults.push(data);
-//     }
-//     if (emptyItem) {
-//       validate(itemName);
-//     }
-//   }
-
-//   displayErrorsMessage();
-//   if (validateResults.length == 0) {
-//     loginStatus = condition.truth;
-//   }
-
-//   if (loginStatus) {
-//     loginForm.style.display = config.displaynone;
-//     maintittle.innerHTML = textData.sucessLogin;
-//   }
-// }
+  if (submitStatus) {
+    submitForm.style.display = config.displaynone;
+    submitForm.submit();
+  }
+}
 
 function animateLable() {
   document.addEventListener(inputTypes.mouseDown, (e) => {
@@ -179,8 +192,8 @@ function animateLable() {
 
 function init() {
   animateLable();
-  // loginButton.addEventListener(inputTypes.click, handleLogin);
-  // resetErrorsMessage();
+  submitButton.addEventListener(inputTypes.click, handleSubmit);
+  resetErrorsMessage();
 }
 
 init();
